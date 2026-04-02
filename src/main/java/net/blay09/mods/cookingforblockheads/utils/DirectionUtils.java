aa2c1c6c17f5eddd3601fefc2c1f0eb56cb361b4
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2026-04-02 10:30:15 -0500

    DirectionUtils splotless-formatting
    + Corrected some spelling errors
    + Re-worded some comments
    + Reformatted several operating (for task splotlessApply)

--- src/main/java/net/blay09/mods/cookingforblockheads/utils/DirectionUtils.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/utils/DirectionUtils.java
@@ -1,45 +1,45 @@
 package net.blay09.mods.cookingforblockheads.utils;
 
 public class DirectionUtils {
+
     /**
-     * Returns the result of the mathematical operation, modulus of two numbers.
-     * This is distinct from Java's "mod" operator, %, which returns the remainder of two numbers;
-     * the mathematic operation, "modulus" returns the remainder, in whole multiples
-     * 
-     * @param number operand
+     * Returns the result of the mathematical-operation "modulus" of any pair of a number and integer. This is distinct
+     * from Java's mod-operator (%), which returns the remainder of any two numbers; the mathematics-operation,
+     * "modulus" returns the remainder, in whole multiples.
+     *
+     * @param number  operand
      * @param modulus zero-congruent definition (maximum space for modular arithmetic)
-     * @return 
+     * @return
      */
     private static double modulus(final double number, final int modulus) {
-        if(number < 0) {
-            return (modulus - (Math.abs(number) % modulus) ) % modulus;
+        if (number < 0) {
+            return number - (modulus * Math.floor(number / modulus));
         } else {
             return number % modulus;
         }
     }
 
     /**
-     * Reformats directions (like an "enum") calculated from the nearest cardinal-directions, as
-     * {South, West, North, East} to {0, 1, 2, 3}
+     * Maps direction, calculated from the nearest cardinal-directions, as {South, West, North, East} to {0, 1, 2, 3}
      *
-     * @param yaw latteral-direction of gaze
+     * @param yaw lateral-direction of gaze
      * @return cardinal-direction index
      */
     public static int getDirection(final double yaw) {
-        double modified_yaw = ( yaw * (4.0/360.0) ) + 0.5;
+        double modified_yaw = (yaw * (4.0 / 360.0)) + 0.5;
         modified_yaw = modulus(modified_yaw, 4);
-        return (int)Math.floor(modified_yaw);
+        return (int) Math.floor(modified_yaw);
     }
 
     /**
-     * Returns the "handedness" of a current gaze, relative to the nearest cardinal-direction,
-     * represented as being before or after (clockwise) the nearest cardinal-direction
+     * Returns the "handedness" of a current gaze, relative to the nearest cardinal-direction, represented as (binary)
+     * being before or after (clockwise) the nearest cardinal-direction
      * 
-     * @param yaw latteral-direction of gaze
+     * @param yaw lateral-direction of gaze
      * @return truth of being before the center of the nearest cardinal-direction
      */
     public static boolean isHalfDirection(final double yaw) {
-        double modified_yaw = yaw * (8.0/360.0);
+        double modified_yaw = yaw * (8.0 / 360.0);
         modified_yaw = modulus(modified_yaw, 2);
         return Math.floor(modified_yaw) == 0;
     }
